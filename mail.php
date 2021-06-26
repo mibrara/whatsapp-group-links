24e9775dcae7ea79a1c8450c68639ed8424cd957
Joni <77877531+mibrara@users.noreply.github.com>
2021-06-26 13:55:11 +0500

    Add files via upload

--- mail.php
+++ mail.php
@@ -3,10 +3,10 @@
        $email = $_POST['email'];
        $message = $_POST['message'];
 
-       $formcontent="Form: $name \n message: $message";
+       $formcontent="From: $name \n message: $message";
        $mailto="naughtymian420@gmail.com";
        $subject="Whatsapp group links submission";
-       $mailheader="Form $email \r\n";
+       $mailheader="From $email \r\n";
        mail ($mailto, $subject, $formcontent, $mailheader) or die ("Error!");
        echo "Thank you"
 ?>
